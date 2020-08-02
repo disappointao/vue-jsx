3912f90ee5c1bbf1238efb93ceff311be62afb3e
ao <1147608791@qq.com>
2020-08-02 21:57:09 +0800

    测试

--- src/components/myForm/index.js
+++ src/components/myForm/index.js
@@ -1,4 +1,4 @@
-import {Input, Form, FormItem, Checkbox, Radio, Select, Button} from 'element-ui'
+import {Input, Form, FormItem, Checkbox, Radio, Select, Button, Option, CheckboxGroup, RadioGroup} from 'element-ui'
 
 export default {
     name: 'myForm',
@@ -10,12 +10,13 @@ export default {
         }
     },
     components: {
-        Input, Form, FormItem, Checkbox, Radio, Select, Button
+        Input, Form, FormItem, Checkbox, Radio, Select, Button,Option,CheckboxGroup,RadioGroup
     },
     data() {
         return {
             formName: '',
-            formModel: {}
+            formModel:{},
+            check:[]
         }
     },
     computed: {},
@@ -24,36 +25,125 @@ export default {
         initForm(itemObj) {
             this.formName = itemObj.name;
             itemObj.items.map((item) => {
-                this.$set(this.formModel, item.name, itemObj.data[item.name]);
+                this.$set(this.formModel, item.name, this.generateFormModelName(item));
             });
         },
-        generateFormItem(itemObj) {
+        generateFormModelName(item,data){
+            try{
+                if(data[item.name]){
+                    return data[item.name]
+                }
+            }catch (e) {
+                if(item.type==='checkbox'){
+                    return []
+                }
+                return null
+            }
+        },
+        generateFormItem(itemsObj) {
             let itemEle = [];
-            itemObj.items.map((item) => {
+            itemsObj.items.map((item) => {
                 switch (item.type) {
                     case 'input':
                         itemEle.push(
-                            <FormItem label={item.label} prop={item.name}>
-                                <Input v-model={this.formModel[item.name]}/>
+                            <FormItem label={item.label} prop={item.name} props = {
+                                {
+                                    ...item.itemAttr
+                                }
+                            }>
+                                <Input v-model={this.formModel[item.name]} placeholder={item.placeholder}/>
                             </FormItem>
-                        )
+                        );
+                         break;
+                    case 'select':
+                        itemEle.push(this.generateSelect(item));
+                        break;
+                    case 'checkbox':
+                        itemEle.push(this.generateCheckbox(item));
+                        break;
+                    case 'radio':
+                        itemEle.push(this.generateRadio(item));
                 }
-            })
+            });
             itemEle.push(
                 <FormItem>
                     <Button type={'primary'} onClick={() => this.$refs[this.formName].resetFields()}>清除</Button>
                 </FormItem>
-            )
+            );
             return itemEle;
+        },
+        generateSelect(selectObj){
+            return (
+              <FormItem label={selectObj.label} prop={selectObj.name}>
+                  <Select
+                    v-model={this.formModel[selectObj.name]}
+                    palceholder={selectObj.placeholder}
+                    style={{width:'100%'}}
+                  >
+                      {
+                          this.generateOption(selectObj)
+                      }
+                  </Select>
+              </FormItem>
+            )
+        },
+        generateCheckbox(checkboxObj){
+            return (
+              <FormItem label={checkboxObj.label} prop={checkboxObj.name}>
+                  <CheckboxGroup
+                        v-model={this.formModel[checkboxObj.name]}
+                        style={{width:'100%',textAlign:'left'}}
+                  >
+                      {
+                          this.generateOption(checkboxObj)
+                      }
+                  </CheckboxGroup>
+              </FormItem>
+            )
+        },
+        generateRadio(radioObj){
+            return (
+              <RadioGroup
+                v-model={this.formModel[radioObj.name]}
+              >
+                  {
+                      this.generateOption(radioObj)
+                  }
+              </RadioGroup>
+            )
+        },
+        generateOption(optionObj){
+            let optionEle = [];
+            optionObj.optionList.map(item=>{
+                switch (optionObj.type) {
+                    case "select":
+                        optionEle.push(
+                          <Option label={item.label} value={item.value}/>
+                        );
+                        break;
+                    case "checkbox":
+                        optionEle.push(
+                          <Checkbox label={item.label} name={item.name}/>
+                        );
+                        break;
+                    case "radio":
+                        optionEle.push(
+                          <Radio label={item.label}/>
+                        );
+                        break;
+                }
+            });
+            return optionEle;
         }
     },
     created() {
-        this.initForm(this.formOption)
+        this.initForm(this.formOption);
+
     },
-    render(h) {
+    render() {
         let ele = [];
         ele = this.generateFormItem(this.formOption);
-        console.log({...this.formOption.option})
+        console.log({...this.formOption.option});
 
         return (
             <div>
--- src/views/baseForm/baseForm.vue
+++ src/views/baseForm/baseForm.vue
@@ -5,44 +5,87 @@
 </template>
 
 <script>
-    import MyForm from "../../components/myForm";
-    export default {
-        name: "baseForm",
-        components:{
-            MyForm
-        },
-        data(){
-            return {
-                formOption:{
-                    name:'channal-form',
-                    data:{
-                    },
-                    items:[
-                        {
-                            label:'姓名',
-                            type:'input',
-                            name:'name'
-                        },
-                        {
-                            label:'账号',
-                            type:'input',
-                            name:'account'
-                        }
-                    ],
-                    rules:
-                        {
-                            name: [{required: true, message: '请输入名字', trigger: 'blur'}],
-                            account: [{required: true, message: '请输入账号', trigger: 'blur'}],
-                        },
-                    buttons:[
+  import MyForm from '../../components/myForm';
 
-                    ],
-                    formAttributes:{
-                    }
+  export default {
+    name: 'baseForm',
+    components: {
+      MyForm,
+    },
+    data() {
+      return {
+        formOption: {
+          name: 'channal-form',
+          data: {},
+          items: [
+            {
+              label: '姓名',
+              type: 'input',
+              name: 'name',
+              placeholder: '请输入姓名',
+              itemAttr: {
+                size: 'mini',
+              },
+            },
+            {
+              label: '账号',
+              type: 'input',
+              name: 'account',
+              placeholder: '请输入账号',
+            },
+            {
+              label: '活动性质',
+              type: 'select',
+              name: 'active',
+              placeholder: '请选择活动性质',
+              optionList: [
+                {
+                  label: '美食/餐厅线上活动',
+                  value: 1,
+                },
+                {
+                  label: '地摊活动',
+                  value: 2,
+                },
+                {
+                  label: '线下主题活动',
+                  value: 3,
+                },
+              ],
+            },
+            {
+              label:'特点',
+              name:'feature',
+              type:'checkbox',
+              optionList: [
+                {
+                  label:'牛批'
+                },
+                {
+                  label:'不牛批'
                 }
+              ]
             }
-        }
-    }
+          ],
+          rules:
+            {
+              name: [{required: true, message: '请输入名字', trigger: 'blur'}],
+              account: [{required: true, message: '请输入账号', trigger: 'blur'}],
+              active: [{required: true, message: '请选择', trigger: 'blur'}],
+              feature:[{required: true, message: '请至少选择一个选项', trigger: 'blur'}],
+            },
+          btnList: [
+            {
+
+            }
+          ],
+          formAttributes: {
+            labelWidth: '120px',
+          },
+        },
+      };
+    },
+  };
 </script>
 
 <style scoped>
